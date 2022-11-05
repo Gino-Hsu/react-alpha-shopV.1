3d36b8e1584f60960454d65387425855554434ef
GinoHsu <ginohsu16@gmail.com>
2022-11-06 00:13:10 +0800

    feat: add RWD in header

--- src/components/header/menu/Menu.js
+++ src/components/header/menu/Menu.js
@@ -6,28 +6,42 @@ import search from '../../../images/icon-search@2x.png'
 import shop from '../../../images/icon-shop@2x.png'
 import theme from '../../../images/icon-toDark@2x.png'
 import logo from '../../../images/Logo@2x.png'
+import hamburger from '../../../images/hamburger@2x.png'
 
 // import css
-import './Menu.css'
+import styles from './Menu.module.css'
 
 export default function Menu() {
   return (
     <>
-      <nav className='navbar-menu'>
-        <ul className='nav-list site-menu-list'>
+      <label className={styles.hamburger} for='menu__checkbox'>
+        <img
+          className={styles.hamburger__img}
+          src={hamburger}
+          alt='Toggle Menu'
+        />
+      </label>
+      <input
+        type='checkbox'
+        id='menu__checkbox'
+        className={styles.menu__checkbox}
+      />
+
+      <nav className={styles.navbar__menu}>
+        <ul className={styles.nav__links}>
           <NavLinks inText='男款' herfLink='#' />
           <NavLinks inText='女款' herfLink='#' />
           <NavLinks inText='最新消息' herfLink='#' />
           <NavLinks inText='客製商品' herfLink='#' />
           <NavLinks inText='聯絡我們' herfLink='#' />
         </ul>
-        <ul className='nav-list site-action-list'>
+        <ul className={styles.nav__icons}>
           <NavIcons imgSrc={search} />
           <NavIcons imgSrc={shop} />
           <NavIcons imgSrc={theme} />
         </ul>
       </nav>
-      <div className='nav-logo'>
+      <div className={styles.nav__logo}>
         <Logo imgSrc={logo} />
       </div>
     </>
