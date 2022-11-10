3e3f20c0931798d785f962ed175cd48a10cbf2ab
GinoHsu <ginohsu16@gmail.com>
2022-11-10 15:17:27 +0800

    refactor: render select's option by mapping & use uuid in key

--- src/components/main/register/forms/Address.jsx
+++ src/components/main/register/forms/Address.jsx
@@ -1,7 +1,47 @@
 import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 import Form from './commonComponents/Form'
 import { InputBox } from './commonComponents/Input'
+
+const APPELLATION = [
+  { value: 'mr', intext: '先生' },
+  { value: 'ms', intext: '女士' },
+  { value: 'mx', intext: '不明' },
+]
+
+const CITY = [
+  { value: null, intext: '請選擇縣市' },
+  { value: 'KLU', intext: '基隆市' },
+  { value: 'TPH', intext: '新北市' },
+  { value: 'TPE', intext: '臺北市' },
+  { value: 'TYC', intext: '桃園市' },
+  { value: 'HSH', intext: '新竹縣' },
+  { value: 'HSC', intext: '新竹市' },
+  { value: 'MAC', intext: '苗栗市' },
+  { value: 'MAL', intext: '苗栗縣' },
+  { value: 'TXG', intext: '臺中市' },
+  { value: 'CWH', intext: '彰化縣' },
+  { value: 'CWS', intext: '彰化市' },
+  { value: 'NTC', intext: '南投市' },
+  { value: 'NTO', intext: '南投縣' },
+  { value: 'YLH', intext: '雲林縣' },
+  { value: 'CHY', intext: '嘉義縣' },
+  { value: 'CYI', intext: '嘉義市' },
+  { value: 'TNN', intext: '臺南市' },
+  { value: 'KHH', intext: '高雄市' },
+  { value: 'IUH', intext: '屏東縣' },
+  { value: 'PTS', intext: '屏東市' },
+  { value: 'ILN', intext: '宜蘭縣' },
+  { value: 'ILC', intext: '宜蘭市' },
+  { value: 'HWA', intext: '花蓮縣' },
+  { value: 'HWC', intext: '花蓮市' },
+  { value: 'TTC', intext: '臺東市' },
+  { value: 'TTT', intext: '臺東縣' },
+  { value: 'PEH', intext: '澎湖縣' },
+  { value: 'KMN', intext: '金門縣' },
+  { value: 'LNN', intext: '連江縣' },
+]
 
 export default function Address() {
   const [selectValue, setSelectValue] = useState({ appellation: '', city: '' })
@@ -25,9 +65,11 @@ export default function Address() {
               onChange={apellationSelectedHandler}
               required
             >
-              <option value='mr'>先生</option>
-              <option value='ms'>女士</option>
-              <option value='mx'>不明</option>
+              {APPELLATION.map(data => (
+                <option key={uuidv4()} value={data.value}>
+                  {data.intext}
+                </option>
+              ))}
             </select>
           </div>
         </InputBox>
@@ -54,36 +96,11 @@ export default function Address() {
               onChange={citySelectedHandler}
               required
             >
-              <option value=''>請選擇縣市</option>
-              <option value='KLU'>基隆市</option>
-              <option value='TPH'>新北市</option>
-              <option value='TPE'>臺北市</option>
-              <option value='TYC'>桃園市</option>
-              <option value='HSH'>新竹縣</option>
-              <option value='HSC'>新竹市</option>
-              <option value='MAC'>苗栗市</option>
-              <option value='MAL'>苗栗縣</option>
-              <option value='TXG'>臺中市</option>
-              <option value='CWH'>彰化縣</option>
-              <option value='CWS'>彰化市</option>
-              <option value='NTC'>南投市</option>
-              <option value='NTO'>南投縣</option>
-              <option value='YLH'>雲林縣</option>
-              <option value='CHY'>嘉義縣</option>
-              <option value='CYI'>嘉義市</option>
-              <option value='TNN'>臺南市</option>
-              <option value='KHH'>高雄市</option>
-              <option value='IUH'>屏東縣</option>
-              <option value='PTS'>屏東市</option>
-              <option value='ILN'>宜蘭縣</option>
-              <option value='ILC'>宜蘭市</option>
-              <option value='HWA'>花蓮縣</option>
-              <option value='HWC'>花蓮市</option>
-              <option value='TTC'>臺東市</option>
-              <option value='TTT'>臺東縣</option>
-              <option value='PEH'>澎湖縣</option>
-              <option value='KMN'>金門縣</option>
-              <option value='LNN'>連江縣</option>
+              {CITY.map(data => (
+                <option key={uuidv4()} value={data.value}>
+                  {data.intext}
+                </option>
+              ))}
             </select>
           </div>
         </InputBox>
