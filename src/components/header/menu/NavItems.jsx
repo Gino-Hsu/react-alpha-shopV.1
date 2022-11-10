def78c14c732d496ca5d585f0be5ffef13fe94c0
GinoHsu <ginohsu16@gmail.com>
2022-11-10 15:26:13 +0800

    refactor: delete '<></>' tag inside NavItems

--- src/components/header/menu/NavItems.jsx
+++ src/components/header/menu/NavItems.jsx
@@ -4,36 +4,30 @@ import styles from './NavItems.module.scss'
 
 export function NavLinks({ inText, herfLink }) {
   return (
-    <>
-      <li className={styles.nav__item}>
-        <div className={`${styles.nav__link} cursor-point`} href={herfLink}>
-          {inText}
-        </div>
-      </li>
-    </>
+    <li className={styles.nav__item}>
+      <div className={`${styles.nav__link} cursor-point`} href={herfLink}>
+        {inText}
+      </div>
+    </li>
   )
 }
 
 export function NavIcons({ imgType, imgAlt, changeTheme }) {
   return (
-    <>
-      <li className={styles.nav__item} onClick={changeTheme}>
-        <div className={`${styles.nav__icon} cursor-point`}>
-          <img className={imgType} alt={imgAlt}></img>
-        </div>
-      </li>
-    </>
+    <li className={styles.nav__item} onClick={changeTheme}>
+      <div className={`${styles.nav__icon} cursor-point`}>
+        <img className={imgType} alt={imgAlt}></img>
+      </div>
+    </li>
   )
 }
 
 export function Logo({ imgAlt }) {
   return (
-    <>
-      <div className='header__logo__container' href='#'>
-        <div className={`${styles.icon__logo} cursor-point`}>
-          <img alt={imgAlt}></img>
-        </div>
+    <div className='header__logo__container' href='#'>
+      <div className={`${styles.icon__logo} cursor-point`}>
+        <img alt={imgAlt}></img>
       </div>
-    </>
+    </div>
   )
 }
